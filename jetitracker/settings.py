5feb3cd4364dcb96760fea000a7050cb99c7014d
martakortslaur <martakortslaur@gmail.com>
2020-01-24 06:21:39 +0000

    deleted requests==2.22.0...

--- jetitracker/settings.py
+++ jetitracker/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 from os import path
 import dj_database_url
-if path.exists("env.py"):
-    import env
+# if path.exists("env.py"):
+#     import env
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -87,12 +87,6 @@ WSGI_APPLICATION = 'jetitracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
