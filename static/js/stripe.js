56510c1119c1fb2f09cee800e1be04bddd4f1d8c
martakortslaur <martakortslaur@gmail.com>
2020-03-06 14:04:48 +0000

    Adding scripts

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -7,20 +7,20 @@ $(function() {
             expYear: $("#id_expiry_year").val(),
             cvc: $("#id_cvv").val()
         };
-    
+        
     Stripe.createToken(card, function(status, response) {
         if (status === 200) {
             $("#credit-card-errors").hide();
             $("#id_stripe_id").val(response.id);
-
-            // Prevent the credit card details from being submitted
-            // to our server
+            
+            //Prevent the Credit card Details from being submitted to our server
             $("#id_credit_card_number").removeAttr('name');
             $("#id_cvv").removeAttr('name');
             $("#id_expiry_month").removeAttr('name');
             $("#id_expiry_year").removeAttr('name');
-
+            
             form.submit();
+            
         } else {
             $("#stripe-error-message").text(response.error.message);
             $("#credit-card-errors").show();
