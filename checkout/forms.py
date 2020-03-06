2cc8437a3a11d9b8329eef8e483c200582873daa
martakortslaur <martakortslaur@gmail.com>
2020-03-06 21:01:38 +0000

    Got stripe working save

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,18 +1,26 @@
 from django import forms
-from .models import Order
+# from django_countries.fields import CountryField
+
 
 class MakePaymentForm(forms.Form):
-    MONTH_CHOICES = [(i,i) for i in range(1, 12)]
-    YEAR_CHOICES = [(i, i) for i in range(2020, 2036)]
-    
-    credit_card_number = forms.CharField(label="Credit card number", required=False)
-    cvv = forms.CharField(label="Security code (CVV)", required=False)
-    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
+    YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
+
+    full_name = forms.CharField(label="Name")
+    credit_card_number = forms.CharField(label='Credit Card Number',
+                                         required=False,
+                                         widget=forms.TextInput(attrs={
+                                             'pattern':
+                                                 '[0-9]{4} *[0-9]{4} *[0-9]{4} *[0-9]{4}',
+                                             'placeholder':
+                                                 '4242 4242 4242 4242',
+                                             'required': True}))
+    cvv = forms.CharField(label='Security Code(CVV)',
+                          min_length=3, required=False,
+                          widget=forms.TextInput(attrs={'required': 'True'}))
+    expiry_year = forms.ChoiceField(label='year',
+                                    choices=YEAR_CHOICES, required=False)
+    expiry_month = forms.ChoiceField(label='month',
+                                     choices=MONTH_CHOICES, required=False)
+    # country = CountryField(default='IE').formfield()
     stripe_id = forms.CharField(widget=forms.HiddenInput)
-    
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields=("full_name", "phone_number", "country", "postcode", 
-                "town_or_city", "street_address1", "street_address2", "county" )
